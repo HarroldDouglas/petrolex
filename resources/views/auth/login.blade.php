b5e075da7b2cf632c51d24874f3c84384aa0206b
Jospin Tedjou <tedjoujos91@gmailcom>
2025-04-21 15:46:56 +0100

    updated login form to old version

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,14 +34,16 @@
                                     <form class="app-form" method="POST" action="{{ route('login') }}">
                                         @csrf
                                         <div class="mb-3 text-center">
-                                            <h3>Bienvenue !</h3>
-                                            <p class="f-s-12 text-secondary">Veuillez vous connecter pour continuer</p>
+                                            <h3>Connectez vous à votre compte</h3>
+                                            <p class="f-s-12 text-secondary">Saisissez vos informations pour vous
+                                                connecter</p>
                                         </div>
 
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text b-r-left text-bg-primary @error('email') is-invalid @enderror" id="basic-addon1">@</span>
-                                            <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control b-r-right" placeholder="email" aria-label="email" aria-describedby="basic-addon1">
-
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">Adresse Email</label>
+                                            <input type="email"
+                                                class="form-control @error('email') is-invalid @enderror" id="email"
+                                                name="email" value="{{ old('email') }}" required autofocus>
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -49,10 +51,11 @@
                                             @enderror
                                         </div>
 
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text b-r-left text-bg-primary @error('password') is-invalid @enderror" id="basic-addon2"><i class="iconoir-lock"></i></span>
-                                            <input type="password" id="password" name="password" value="" class="form-control b-r-right" placeholder="mot de passe" aria-label="password" aria-describedby="basic-addon1">
-
+                                        <div class="mb-3">
+                                            <label for="password" class="form-label">Mot de passe</label>
+                                            <input type="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                id="password" name="password" required>
                                             @error('password')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -64,14 +67,12 @@
                                             <input type="checkbox" class="form-check-input" id="remember"
                                                 name="remember" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label" for="remember">Se souvenir de moi</label>
-                                            <a href="#" class="link-primary float-end">Mot de passe oublié</a>
                                         </div>
 
                                         <div>
                                             <button type="submit" class="btn btn-primary w-100">Connexion</button>
                                         </div>
                                     </form>
-
                                 </div>
                             </div>
                         </div>
