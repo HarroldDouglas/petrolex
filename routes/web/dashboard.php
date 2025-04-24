a3a8e79bec47008dde5f47a304c8db738f04d944
Jospin Tedjou <tedjoujos91@gmailcom>
2025-04-24 15:42:45 +0100

    transaction view

--- routes/web/dashboard.php
+++ routes/web/dashboard.php
@@ -5,12 +5,14 @@ use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Dashboard\StockMovementController;
 use App\Http\Controllers\Dashboard\FinancialReportController;
 use App\Http\Controllers\Dashboard\OrderReportController;
+use App\Http\Controllers\Dashboard\TransactionReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/tableau-de-bord', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/performance-point-de-distribution', StockSaleDashboardController::class)->name('warehouse-stock-dashboard');
-    Route::get('/mouvement-de-stock', StockMovementController::class)->name('stock-movement');
-    Route::get('/rapport-financier', FinancialReportController::class)->name('financial-report');
-    Route::get('/rapport-commande', OrderReportController::class)->name('order-report');
+    Route::get('/performance-point-de-distribution', StockSaleDashboardController::class)->name('warehouse_stock_dashboard');
+    Route::get('/mouvement-de-stock', StockMovementController::class)->name('stock_movement');
+    Route::get('/rapport-financier', FinancialReportController::class)->name('financial_report');
+    Route::get('/rapport-commande', OrderReportController::class)->name('order_report');
+    Route::get('/rapport-transaction', TransactionReportController::class)->name('transaction_report');
 });
