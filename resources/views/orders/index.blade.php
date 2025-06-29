7ae94b4213514ef3265131f2db99a44a7bf2dfcd
douglaswafo97 <harroldwafo@gmail.com>
2025-06-29 15:32:26 +0100

    Fixed moment.min.js not found

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -60,6 +60,4 @@
         <!-- api js -->
         <script src="{{ asset('assets/js/ticket.js') }}"></script>
 
-        <script src="{{ asset('assets/vendor/moment/moment.min.js') }}"></script>
-
     @endsection
