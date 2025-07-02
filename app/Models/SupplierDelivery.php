b6637e764272872109eb24fec0b8a3778900393c
douglaswafo97 <harroldwafo@gmail.com>
2025-07-02 11:08:59 +0100

    Small fixed

--- app/Models/SupplierDelivery.php
+++ app/Models/SupplierDelivery.php
@@ -161,6 +161,6 @@ class SupplierDelivery extends Model
      */
     public function canBeEdited(): bool
     {
-        return $this->status->value === 'in_progress';
+        return $this->status->value === SupplierDeliveryStatus::IN_PROGRESS();
     }
 }
