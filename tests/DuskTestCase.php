e7f997677822d507362d5f5ba5e0f31b14e60172
douglaswafo97 <harroldwafo@gmail.com>
2025-07-03 15:59:31 +0100

    Add first laravel dusk test

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,47 +2,49 @@
 
 namespace Tests;
 
-use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Firefox\FirefoxOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
-use Illuminate\Support\Collection;
 use Laravel\Dusk\TestCase as BaseTestCase;
-use PHPUnit\Framework\Attributes\BeforeClass;
 
 abstract class DuskTestCase extends BaseTestCase
 {
-    /**
-     * Prepare for Dusk test execution.
-     */
-    #[BeforeClass]
-    public static function prepare(): void
+    use CreatesApplication;
+
+    protected function setUp(): void
     {
-        if (! static::runningInSail()) {
-            static::startChromeDriver(['--port=9515']);
-        }
+        parent::setUp();
     }
 
     /**
-     * Create the RemoteWebDriver instance.
+     * Configuration Firefox Driver
      */
     protected function driver(): RemoteWebDriver
     {
-        $options = (new ChromeOptions)->addArguments(collect([
-            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
-            '--disable-search-engine-choice-screen',
-            '--disable-smooth-scrolling',
-        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
-            return $items->merge([
+        $options = (new FirefoxOptions)
+            ->addArguments([
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--window-size=1920,1080',
                 '--disable-gpu',
-                '--headless=new',
+                '--disable-web-security',
             ]);
-        })->all());
+
+        $capabilities = DesiredCapabilities::firefox();
+        $capabilities->setCapability(FirefoxOptions::CAPABILITY, $options);
 
         return RemoteWebDriver::create(
-            $_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:9515',
-            DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
-            )
+            'http://127.0.0.1:4444',
+            $capabilities
         );
+    }
+
+    /**
+     * Nettoyer après chaque test
+     */
+    protected function tearDown(): void
+    {
+        static::closeAll();
+        parent::tearDown();
     }
 }
