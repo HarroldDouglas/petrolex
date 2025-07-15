d9877bc611578a12bc5b67c4939a680ca74b4490
Jospin <tedjoujos91@gmail.com>
2025-07-15 14:16:23 +0100

    added API documentation

---
+++ app/DTOs/Customer/CustomerDeliveryAddressDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DTOs\Customer;
+
+use App\DTOs\BaseDTO;
+
+class CustomerDeliveryAddressDTO extends BaseDTO
+{
+    public function __construct(
+        public string $label,
+        public string $address,
+        public ?string $neighborhood,
+        public ?string $city,
+        public ?string $country,
+        public ?float $latitude,
+        public ?float $longitude,
+        public ?string $phone,
+        public ?string $contact_firstname,
+        public ?string $contact_lastname,
+        public ?string $email,
+        public ?string $address_precision,
+        public ?bool $is_default,
+    ) {}
+}
--- app/Http/Api/Controllers/Customer/StoreCustomerDeliveryAddressController.php
+++ app/Http/Api/Controllers/Customer/StoreCustomerDeliveryAddressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Api\Controllers\Customer;
 
+use App\DTOs\Customer\CustomerDeliveryAddressDTO;
 use App\Http\Api\Responses\Customer\StoreCustomerDeliveryAddressResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Customer\StoreCustomerDeliveryAddressRequest;
@@ -14,7 +15,8 @@ class StoreCustomerDeliveryAddressController extends Controller
      */
     public function __invoke(StoreCustomerDeliveryAddressRequest $request, Customer $customer): StoreCustomerDeliveryAddressResponse
     {
-        $deliveryAddress = $customer->deliveryAddresses()->create($request->validated());
+        $dto = CustomerDeliveryAddressDTO::from($request->validated());
+        $deliveryAddress = $customer->deliveryAddresses()->create($dto->toArray());
 
         return StoreCustomerDeliveryAddressResponse::withAddress($deliveryAddress);
     }
