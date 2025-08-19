a366d1d06743fc17c1817892cd44dd3a32b2eb08
Jospin <tedjoujos91@gmail.com>
2025-08-19 13:11:50 +0100

    added via method on notifiation to specify notification channel

---
+++ app/Notifications/PasswordUpdatedNotification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\User\PasswordUpdatedMail;
+use App\Models\User;
+use Illuminate\Notifications\Notification;
+
+final class PasswordUpdatedNotification extends Notification
+{
+    public function __construct(
+        public readonly User $user,
+    ) {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): PasswordUpdatedMail
+    {
+        return new PasswordUpdatedMail($this->user)
+                ->to($notifiable->email);
+    }
+}
