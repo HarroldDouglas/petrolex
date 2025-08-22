44e7604f88f0082160f5a8725183240602bc445f
Jospin <tedjoujos91@gmail.com>
2025-08-22 18:33:49 +0100

    i=updated swagger url

--- documentation/Auth/base/ApiInfo.php
+++ documentation/Auth/base/ApiInfo.php
@@ -4,23 +4,17 @@ use OpenApi\Annotations as OA;
 
 /**
  * @OA\Info(
- *     title="API Documentation",
- *     version="1.0.0",
- *     description="Documentation de l'API",
- *
+ *     title="Petrolex API",
+ *     version="1.0",
+ *     description="Documentation API pour Petrolex",
  *     @OA\Contact(
  *         email="contact@example.com"
  *     )
  * )
  *
  * @OA\Server(
- *     url="http://localhost:8000",
- *     description="Local Development Server"
- * )
- *
- * @OA\Server(
- *     url="https://api.example.com",
- *     description="Production Server"
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
  * )
  *
  * @OA\SecurityScheme(
