52c5d6827c829e6bf0c81c975787fa405ca0661d
Jospin <tedjoujos91@gmail.com>
2025-09-01 11:47:14 +0100

    support contact endpoint created

--- routes/api/app.php
+++ routes/api/app.php
@@ -2,9 +2,12 @@
 
 use App\Http\Api\Controllers\App\GetTermsAndConditionsController;
 use App\Http\Api\Controllers\App\PreviewTermsAndConditionsController;
+use App\Http\Api\Controllers\App\GetSupportContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('app')->name('api.app.')->group(function () {
     Route::get('/terms-and-conditions', GetTermsAndConditionsController::class)->name('terms-and-conditions');
     Route::get('/terms-and-conditions/preview', PreviewTermsAndConditionsController::class)->name('terms-and-conditions.preview');
+    
+    Route::get('/support/contact', GetSupportContactController::class)->name('support.contact');
 });
