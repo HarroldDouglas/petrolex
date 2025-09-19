beab77d0eb07f19fb61a749aca9f1cd99b3d7d87
HarroldDouglas <harroldwafo@gmail.com>
2025-09-19 20:51:49 +0100

    Fixed deliver_address consistency

--- app/Http/Api/Responses/Auth/ProfileResponse.php
+++ app/Http/Api/Responses/Auth/ProfileResponse.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Api\Responses\Auth;
 
+use App\Enums\UserRole;
+use App\Http\Api\Resources\CustomerResource;
 use App\Http\Api\Resources\UserResource;
 use App\Http\Api\Responses\ApiResponse;
 use App\Models\User;
@@ -15,8 +17,13 @@ class ProfileResponse extends ApiResponse
             'customer.deliveryAddresses.neighborhood.municipality.city.country',
         ]);
 
+        $userResource = match (true) {
+            $user->hasRole(UserRole::CUSTOMER()->value) => new CustomerResource($user->customer),
+            default => new UserResource($user),
+        };
+
         return new self(
-            new UserResource($user),
+            $userResource,
             'Profil récupéré avec succès'
         );
     }
