2e5405cdf1213824bad55712be479463a1b2fcfb
Jospin <tedjoujos91@gmail.com>
2025-10-15 12:45:44 +0100

    Fix merge conflict in payment status  verificatio Job

--- app/Jobs/VerifyPaymentStatusJob.php
+++ app/Jobs/VerifyPaymentStatusJob.php
@@ -242,12 +242,8 @@ class VerifyPaymentStatusJob implements ShouldQueue
             'delay' => self::CHECK_INTERVAL.' seconds',
         ]);
 
-<<<<<<< HEAD
         dispatch((new self($this->referenceId, $this->paymentMethod, $this->paymentService, 
                     $nextAttempt))->delay(now()->addSeconds(self::CHECK_INTERVAL)));
-=======
-        dispatch((new self($this->referenceId, $this->paymentMethod, $this->paymentService, $nextAttempt))->delay(now()->addSeconds(self::CHECK_INTERVAL)));
->>>>>>> dev
     }
 
     /**
