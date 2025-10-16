23c47cea51f9e19a88da189775f5644d6226b712
Jospin <tedjoujos91@gmail.com>
2025-10-16 10:07:55 +0100

    Fixed RoleRepository not resolving issue

--- app/Services/Role/RoleService.php
+++ app/Services/Role/RoleService.php
@@ -16,7 +16,7 @@ class RoleService extends BaseServiceForEntity
         protected RoleRepositoryInterface $roleRepository,
         protected PermissionService $permissionService
     ) {
-        parent::__construct($this->roleRepository);
+        parent::__construct($roleRepository);
     }
 
     protected function getModel(): string
